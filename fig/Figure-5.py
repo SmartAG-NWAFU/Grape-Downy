679ec6a94d2bd5eab714e26e85e6f09402ff89f6
husnxjsjsb <102045635+husnxjsjsb@users.noreply.github.com>
2026-04-08 17:51:34 +0800

    Update Figure-5.py

--- fig/Figure-5.py
+++ fig/Figure-5.py
@@ -97,7 +97,7 @@ def plot_model_comparison_bar():
                fontsize=9, ncol=6, frameon=False)
 
     plt.tight_layout(pad=0.4, rect=[0, 0, 1, 0.95])
-    plt.savefig(r"Fig_5.png",
+    plt.savefig(r"Figure-5.jpg",
                 dpi=300, bbox_inches='tight')
     plt.show()
 
